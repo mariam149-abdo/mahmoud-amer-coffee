dd4411733842232cac809616eeaea902d2fce1b7
mariam <mariamabdelhaq883@gmail.com>
2025-07-23 17:14:12 +0300

    edit all

--- script.js
+++ script.js
@@ -52,3 +52,19 @@ prevBtn.addEventListener('click', () => {
   const scrollAmount = getItemScrollWidth();
   container.scrollBy({ left: isRTL ? scrollAmount : -scrollAmount, behavior: 'smooth' });
 });
+
+const menu = document.getElementById("mobile-menu");
+const menubtn= document.getElementById("menu-icon");
+const menua =document.querySelectorAll(".mobile-menu a");
+const animation ="animation-menu";
+menubtn.addEventListener("click", () => {
+ if (menu.style.display === "flex") {
+    menu.style.display = "none";
+    menua.forEach(link =>{link.style.display="none"});
+  } 
+  else {
+    menu.style.display = "flex";
+    menua.forEach(link =>{link.style.display="flex"});
+    menu.classList.add(animation);
+  }
+})  
